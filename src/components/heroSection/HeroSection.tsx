aeb0644e86bfdfac6622cbfd7818615458b66256
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-06-30 18:23:17 +0300

    finish hero section

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -13,9 +13,9 @@ const HeroSection = () => {
   const locale = useLocale();
   const { resolvedTheme } = useTheme();
   return (
-    <section className="h-[calc(100vh-64px)] py-4 md:py-12 lg:py-16 xl:py-24 relative overflow-hidden">
+    <section className="h-[calc(100vh-64px)] py-10 md:py-12 lg:py-16 xl:py-24 relative overflow-hidden">
       <div className="container relative">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 py-4 lg:py-12 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 py-4 lg:py-12 ">
           <div className="flex flex-col gap-3 py-2 md:py-20">
             <h1 className="text-2xl md:text-4xl font-bold">{t("name")}</h1>
             <p className="text-xl md:text-2xl font-bold">- {t("jobName")}</p>
@@ -37,7 +37,7 @@ const HeroSection = () => {
           </div>
         </div>
         <div
-          className={`absolute bottom-0 mb-[-40px] md:mb-0 z-10 ${
+          className={`absolute bottom-0 mb-[-60px] md:mb-0 z-10 ${
             locale === "en" ? "left-4" : "right-4"
           } scroll-animation`}
         >
