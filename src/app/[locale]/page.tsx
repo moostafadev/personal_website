90321ad45d26e27b60a98e43ce93850212bc8040
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-07-20 16:15:33 +0300

    remove .env file

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,3 @@
-import { getAllProjectsAction } from "@/actions/project.action";
 import AboutSection from "@/components/AboutSection";
 import ExperienceSection from "@/components/ExperienceSection";
 import Header from "@/components/Header";
@@ -6,8 +5,6 @@ import HeroSection from "@/components/heroSection/HeroSection";
 import SkillsSection from "@/components/SkillsSection";
 
 export default async function Index() {
-  const projects = await getAllProjectsAction();
-  console.log(projects);
   return (
     <main>
       <Header />
